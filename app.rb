07a160847452e736c8fa8266bf2012c4358ccaae
diego marin <dilema4000@gmail.com>
2022-09-03 20:25:23 -0500

    Sinatra

--- app.rb
+++ app.rb
@@ -3,4 +3,57 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+configure :development do
+  use Rack::MethodOverride # habilita metodos delete y patch
+end
 
+get "/" do
+
+
+
+end
+
+get "/restaurants" do
+  @restaurants = Restaurant.all
+
+  erb :index
+end
+
+get "/restaurants/new" do # new
+
+  erb :new
+end
+
+post "/restaurants" do # RUTA QUE GUARDA datos nuevos
+  Restaurant.create(
+    name: params[:name],
+    city: params[:city],
+    address: params[:address],
+    type_food: params[:type_food],
+    phone_number: params[:phone_number],
+    image_url: params[:image_url]
+  )
+
+  redirect to "/restaurants"
+
+end
+
+
+get "/restaurants/:id" do #show
+  @restaurant = Restaurant.find(params[:id])
+
+  erb :show
+end
+
+
+get "/restaurants /:id/edit" do
+
+end
+
+patch "/restaurants/:id" do # guarda los datos modificados, boton guardar
+
+end
+
+delete "/restaurants/:id" do
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+puts "Creando restaurantes"
 
+Restaurant.create(
+  name: "Los buenos muchachos",
+  address: "Av, Brasil #35",
+  city: "Medellin",
+  image_url:"https://www.elespectador.com/resizer/l9DpvN_dUsKm1WRGlNBvvlgPb0I=/968x645/filters:format(jpeg)/cloudfront-us-east-1.images.arcpublishing.com/elespectador/6W4NBCLZ75DEFBCEUZHYDCNDZI.jpg",
+  phone_number:"987676543",
+  type_food:"Comida Española"
+)
+
+Restaurant.create(
+  name: "Los malos muchachos",
+  address: "Av, Colombia #35",
+  city: "Medellin",
+  image_url:"https://www.eltiempo.com/files/article_main_mobile/uploads/2021/09/30/61565a62e9dc3.jpeg",
+  phone_number:"987458777",
+  type_food:"Comida Mexicana"
+)
